b37d9d8f83b57173994ef7ad3e367f8d5866dde3
Riki420 <girottiriccardo1998@gmail.com>
2021-07-22 14:29:23 +0200

    analisi carta

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// ┌──────────────────────────────────────────────────────────────────────────────┐
+// │                          Analisi Carta                                       │
+// └──────────────────────────────────────────────────────────────────────────────┘
+//* 1 NOME CARTA
+//* 2 COSTO MANA
+//* 3 TIPOLOGIA DELLA CARTA
+//* 4 NUMERO ESPANSIONE
+//* 5 ABILITA'
+//* 6 TESTO SECONDARIO
+//* 7 FORZA E COSTITUZIONE
+//* 8 NUMERO DELLA COLLEZIONE
+//* 9 ILLUSTRAZIONE E ILLUSTRATORE
+
+//  --------------------------------------------------------------------------------------
+
