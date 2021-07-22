a468713f2a1555f1b3885acc04c7b15953c0d211
Riki420 <girottiriccardo1998@gmail.com>
2021-07-22 15:01:58 +0200

    creato oggetto della carta + proprietà

--- js/script.js
+++ js/script.js
@@ -13,3 +13,52 @@
 
 //  --------------------------------------------------------------------------------------
 
+const card = {
+    id: 1,
+    name: 'Bloodfire Colossus',
+    manaCost: ['6','R', 'R'],
+    convertedCost: 8,
+    cardType: 'Creature',
+    subType: 'Giant',
+    expansion: {
+        reprintId: 9,
+        name: 'Ninth Edition',
+        rarity: 'Rare'
+    },
+
+    abilities: [
+        {
+            activeCost: ['R', 'T'],
+            description: 'bla bla bla'
+        },
+        {
+            activeCost: ['R', 'R', 'T'],
+            description: 'bla bla'
+        }
+    ],
+
+    flavourText: {
+        quote: 'bla in corsivo',
+        author: 'John Fitzgerald'
+    },
+    collectionNr: '124/432',
+    constitution: '4',
+    streight: '2',
+    borderColor: '#000',
+
+    illustration: {                           
+        author: {
+            id: 1,                            
+            name: 'Marchino'                   
+        },
+        source: 'img/pic.jpg'              
+    },
+
+    background: {                              
+        color: 'red',
+        source: 'img/pic.jpg'
+    }
+
+}
+
+console.table(card);
