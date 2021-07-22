13a31500602adb3664a3657664220d7f7b1a4108
Riki420 <girottiriccardo1998@gmail.com>
2021-07-22 17:54:50 +0200

    aggiunta una finta sezione filtri tanto per

--- js/script.js
+++ js/script.js
@@ -73,41 +73,41 @@ PrintCard(card);
 //*│                           Dichiarazione Funzione                             │
 // └──────────────────────────────────────────────────────────────────────────────┘
 
-function PrintCard(carta){
+function PrintCard(obj){
     const cardSection = document.getElementById('card');
 
     //se non dovesse esserci un subType crea una stringa vuota
-    const subType = carta.subType ? `- ${carta.subType} ` : '';
+    const subType = obj.subType ? `- ${obj.subType} ` : '';
 
     //Se non ci sono effetti mostra il messaggio nel caso contrario gira dentro l'array delle abilità e stampane il contenuto
     let abilitiesContent = '<em>No abilities</em>';
-    if(carta.abilities.length){
+    if(obj.abilities.length){
     abilitiesContent = '<ul class="sublist">';
-    for(let i = 0; i < carta.abilities.length; i++){
-        const currentAbilities = carta.abilities[i];
+    for(let i = 0; i < obj.abilities.length; i++){
+        const currentAbilities = obj.abilities[i];
         abilitiesContent += `<li><strong>Description:</strong> ${currentAbilities.description} <strong></li><li>Effect Cost:</strong> ${currentAbilities.activeCost}</li>`
     }
     abilitiesContent += '</ul>'
 }
     let cardTemplate = `
     <ul class="card">
-        <li><strong>Nome:</strong> ${carta.name}</li>
-        <li><strong>Mana Cost:</strong> ${carta.manaCost}</li>
-        <li><strong>Converted Mana Cost:</strong> ${carta.convertedCost}</li>
-        <li><strong>Type:</strong> ${carta.cardType} ${subType}</li>
+        <li><strong>Nome:</strong> ${obj.name}</li>
+        <li><strong>Mana Cost:</strong> ${obj.manaCost}</li>
+        <li><strong>Converted Mana Cost:</strong> ${obj.convertedCost}</li>
+        <li><strong>Type:</strong> ${obj.cardType} ${subType}</li>
         <li><strong>Expansion:</strong>
             <ul class="sublist">
-                <li>Reprint: ${carta.expansion.reprintId}</li>
-                <li>Name: ${carta.expansion.name}</li>
-                <li>Rarity: ${carta.expansion.rarity}</li>
-                <li>Collection Num: ${carta.collectionNr}</li>
+                <li>Reprint: ${obj.expansion.reprintId}</li>
+                <li>Name: ${obj.expansion.name}</li>
+                <li>Rarity: ${obj.expansion.rarity}</li>
+                <li>Collection Num: ${obj.collectionNr}</li>
             </ul>
         </li>  
         <li><strong>Abilities:</strong> ${abilitiesContent}</li>
-        <li><strong>Flavour Text:</strong> ${carta.flavourText.quote} - <em>${carta.flavourText.author}</em></li>
-        <li><strong>Toughness/Strenght:</strong> ${carta.toughness}/${carta.strength}</li>
-        <li><strong>Illustrator:</strong><em> ${carta.illustration.author}<em></li>
-        <li><strong>Illustration:</strong><br> <img src="${carta.illustration.source}"> </li>
+        <li><strong>Flavour Text:</strong> ${obj.flavourText.quote} - <em>${obj.flavourText.author}</em></li>
+        <li><strong>Toughness/Strenght:</strong> ${obj.toughness}/${obj.strength}</li>
+        <li><strong>Illustrator:</strong><em> ${obj.illustration.author}<em></li>
+        <li><strong>Illustration:</strong><br> <img src="${obj.illustration.source}"> </li>
     
     </ul>
     `;
