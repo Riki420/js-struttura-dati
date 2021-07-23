2382e7e30a711848f68627e57f34029e341596df
Riki420 <girottiriccardo1998@gmail.com>
2021-07-23 15:45:05 +0200

    Creato array di obj + stampa obj

--- js/script.js
+++ js/script.js
@@ -14,7 +14,9 @@
 //  --------------------------------------------------------------------------------------
 
 
-const card = {
+const deck = [
+
+{
     id: 1,
     name: 'Bloodfire Colossus',
     manaCost: ['6','R', 'R'],
@@ -53,17 +55,107 @@ const card = {
         source: 'img/pic.jpg'
     }
 
-}
+},
+{
+    id: 2,
+    name: 'Body Double',
+    manaCost: ['4','U'],
+    convertedCost: 5,
+    cardType: 'Creature',
+    subType: 'Shapeshifter',
+    expansion: {
+        name: 'Planar Chaos',
+        rarity: 'Rare'
+    },
 
-console.table(card);
+    abilities: [
+        {
+            activeCost: '0',
+            description: 'You may have Body Double enter the battlefield as a copy of any creature card in a graveyard.'
+        }
+    ],
+
+    flavourText: {
+        quote: 'Where the dead outnumbered the living, mimics scavenged faces from the fallen.',
+        author: ''
+    },
+    collectionNr: '35/432',
+    toughness: '0',
+    strength: '0',
+    borderColor: '#000',
+
+    illustration: {                           
+        author: 'Greg Staples',
+        source: 'img/pic2.png'              
+    },
+
+    background: {                              
+        color: 'red',
+        source: 'img/pic.jpg'
+    }
+
+},
+{
+    id: 3,
+    name: 'Bathe in Dragonfire',
+    manaCost: ['2','R'],
+    convertedCost: 3,
+    cardType: 'Sorcery',
+    subType: '',
+    expansion: {
+        name: 'Fate Reforged',
+        rarity: 'Common'
+    },
+
+    abilities: [
+        {
+            activeCost: '0',
+            description: 'Bathe in Dragonfire deals 4 damage to target creature.'
+        }
+    ],
+
+    flavourText: {
+        quote: "L'odore di carne bruciata aleggia nel paesaggio annerito di Tarkir.",
+        author: ''
+    },
+    collectionNr: '92/432',
+    toughness: '',
+    strength: '',
+    borderColor: '#000',
+
+    illustration: {                           
+        author: 'Chris Rallis',
+        source: 'img/pic3.png'              
+    },
+
+    background: {                              
+        color: 'red',
+        source: 'img/pic.jpg'
+    }
+
+}
+];
+
+
+
+
 
 // ┌────────────────────────────────────────────────────────────────┐
 //*│                       Print on Page                            │
 // └────────────────────────────────────────────────────────────────┘
 
+const cardSection = document.getElementById('card');
 
-PrintCard(card);
+let deckTemplate = '';
 
+for(var i = 0; i < deck.length; i++){
+    const currentCard = deck[i];
+    const currentCardTemplate = PrintCard(currentCard);
+    deckTemplate += currentCardTemplate
+};
+
+
+cardSection.innerHTML = deckTemplate;
 
 
 
@@ -74,7 +166,6 @@ PrintCard(card);
 // └──────────────────────────────────────────────────────────────────────────────┘
 
 function PrintCard(obj){
-    const cardSection = document.getElementById('card');
 
     //se non dovesse esserci un subType crea una stringa vuota
     const subType = obj.subType ? `- ${obj.subType} ` : '';
@@ -90,27 +181,28 @@ function PrintCard(obj){
     abilitiesContent += '</ul>'
 }
     let cardTemplate = `
-    <ul class="card">
-        <li><strong>Nome:</strong> ${obj.name}</li>
-        <li><strong>Mana Cost:</strong> ${obj.manaCost}</li>
-        <li><strong>Converted Mana Cost:</strong> ${obj.convertedCost}</li>
-        <li><strong>Type:</strong> ${obj.cardType} ${subType}</li>
-        <li><strong>Expansion:</strong>
-            <ul class="sublist">
-                <li>Reprint: ${obj.expansion.reprintId}</li>
-                <li>Name: ${obj.expansion.name}</li>
-                <li>Rarity: ${obj.expansion.rarity}</li>
-                <li>Collection Num: ${obj.collectionNr}</li>
-            </ul>
-        </li>  
-        <li><strong>Abilities:</strong> ${abilitiesContent}</li>
-        <li><strong>Flavour Text:</strong> ${obj.flavourText.quote} - <em>${obj.flavourText.author}</em></li>
-        <li><strong>Toughness/Strenght:</strong> ${obj.toughness}/${obj.strength}</li>
-        <li><strong>Illustrator:</strong><em> ${obj.illustration.author}<em></li>
-        <li><strong>Illustration:</strong><br> <img src="${obj.illustration.source}"> </li>
-    
-    </ul>
+        <ul class="card">
+            <li><strong>Nome:</strong> ${obj.name}</li>
+            <li><strong>Mana Cost:</strong> ${obj.manaCost}</li>
+            <li><strong>Converted Mana Cost:</strong> ${obj.convertedCost}</li>
+            <li><strong>Type:</strong> ${obj.cardType} ${subType}</li>
+            <li><strong>Expansion:</strong>
+                <ul class="sublist">
+                    <li>Reprint: ${obj.expansion.reprintId}</li>
+                    <li>Name: ${obj.expansion.name}</li>
+                    <li>Rarity: ${obj.expansion.rarity}</li>
+                    <li>Collection Num: ${obj.collectionNr}</li>
+                </ul>
+            </li>  
+            <li><strong>Abilities:</strong> ${abilitiesContent}</li>
+            <li><strong>Flavour Text:</strong> ${obj.flavourText.quote} - <em>${obj.flavourText.author}</em></li>
+            <li><strong>Toughness/Strenght:</strong> ${obj.toughness}/${obj.strength}</li>
+            <li><strong>Illustrator:</strong><em> ${obj.illustration.author}<em></li>
+            <li><strong>Illustration:</strong><br> <img src="${obj.illustration.source}"> </li>
+        
+        </ul> 
     `;
-    cardSection.innerHTML = cardTemplate;
+    return cardTemplate;
+
 
 }
